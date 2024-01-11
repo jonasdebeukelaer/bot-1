5927af236e9f286ff27a2b372df2d8eb268a3859
jonasdebeukelaer <jonas.db@live.co.uk>
2024-01-11 10:43:43 +0545

    feat: log full msg sent to LLM

--- src/bot/llm_trader.py
+++ src/bot/llm_trader.py
@@ -1,6 +1,7 @@
 from typing import Dict, List, Any
 
 from llm_interface import LLMInterface
+from logger import logger
 
 
 class Trader(LLMInterface):
@@ -12,15 +13,15 @@ class Trader(LLMInterface):
         last_trades: List[str],
         order_book: Dict[str, Any],
     ) -> Dict[str, Any]:
+        system_message = "You are an advanced swing trader with a medium-high risk appetite. You've been trading Bitcoin and other cryptocurrencies, leveraging your expertise to capitalize on market trends while managing risks. Provide recommendations avoiding things like FOMO and FUD. You are requested to make a decision once an hour, so take this into account when making your decision. Make sure to think step by stepin the reasoning property before making a decision."
+
+        user_message = f"Your trading porfolio breakdown: {portfolio_breakdown} \n\n Your last 20 trades within the last 7 days: {last_trades} \n\nhourly price and indicators of bitcoin: {indicator_history_hourly} \n\ndaily price and indicators of bitcoin: {indicator_history_daily} \n\nkucoin order book (20 pieces): {order_book} \n\nGiven your portfolio, the price and indicators history of bitcoin provided and your last trades, what is your trading decision?"
+
+        logger.log_info("Message sent to LLM: " + user_message)
+
         messages = [
-            {
-                "role": "system",
-                "content": "You are an advanced swing trader with a medium-high risk appetite. You've been trading Bitcoin and other cryptocurrencies, leveraging your expertise to capitalize on market trends while managing risks. Provide recommendations avoiding things like FOMO and FUD. You are requested to make a decision once an hour, so take this into account when making your decision. Make sure to think step by stepin the reasoning property before making a decision.",
-            },
-            {
-                "role": "user",
-                "content": f"Your trading porfolio breakdown: {portfolio_breakdown} \n\n Your last 20 trades within the last 7 days: {last_trades} \n\nhourly price and indicators of bitcoin: {indicator_history_hourly} \n\ndaily price and indicators of bitcoin: {indicator_history_daily} \n\nkucoin order book (20 pieces): {order_book} \n\nGiven your portfolio, the price and indicators history of bitcoin provided and your last trades, what is your trading decision?",
-            },
+            {"role": "system", "content": system_message},
+            {"role": "user", "content": user_message},
         ]
 
         function = {
