13377cf3c7d7851f65a643ed11bc6e8e3f01efdc
jonasdebeukelaer <jonas.db@live.co.uk>
2024-06-29 11:15:18 +0100

    refactor(bot/utils): use singledispatch

--- src/bot/util.py
+++ src/bot/util.py
@@ -1,11 +1,20 @@
+from functools import singledispatch
 from typing import Any
 
 
+@singledispatch
 def five_sig_fig(val: Any) -> str:
-    """
-    formats to 5 significant figures
-    """
-    if isinstance(val, (float, int)) or (isinstance(val, str) and val.replace(".", "", 1).isdigit()):
-        return f"{float(val):.5g}"
+    return val
 
+
+@five_sig_fig.register(float)
+@five_sig_fig.register(int)
+def _(val: float) -> str:
+    return f"{val:.5g}"
+
+
+@five_sig_fig.register(str)
+def _(val: str) -> str:
+    if val.replace(".", "", 1).isdigit():
+        return f"{float(val):.5g}"
     return val
