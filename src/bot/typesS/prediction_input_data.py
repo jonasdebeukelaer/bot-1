7d49c3f37971a47d0e663c264b9d8befd6846610
jonasdebeukelaer <jonas.db@live.co.uk>
2024-07-05 16:13:45 +0100

    feat: add target_ts to input

--- src/bot/typesS/prediction_input_data.py
+++ src/bot/typesS/prediction_input_data.py
@@ -11,6 +11,7 @@ class PredictionInputData:
     target_timedelta: timedelta
     ts: datetime = field(default_factory=datetime.now)
     ts_str: str = field(init=False)
+    target_ts: datetime = field(init=False)
     target_ts_str: str = field(init=False)
     target_td_str: str = field(init=False)
 
@@ -22,5 +23,6 @@ class PredictionInputData:
             raise ValueError(f"Timestamp must be a datetime object, got {type(self.ts)}")
 
         self.ts_str = self.ts.strftime("%Y-%m-%d %H:%M:%S")
-        self.target_ts_str = (self.ts + self.target_timedelta).strftime("%Y-%m-%d %H:%M:%S")
+        self.target_ts = self.ts + self.target_timedelta
+        self.target_ts_str = self.target_ts.strftime("%Y-%m-%d %H:%M:%S")
         self.target_td_str = str(self.target_timedelta)
